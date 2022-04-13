f3679358c7ad63d14f8c860c30b0363ac845a04c
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-04-13 08:09:12 -0400

    Added a flag.

--- kill_process.ps1
+++ kill_process.ps1
@@ -1,4 +1,4 @@
 #Asking the user to enter the PID of the process they would like to kill.
-$PID = Read-Host "Please enter the PID that you would like to kill"
+$PID = Read-Host -Prompt "Please enter the PID that you would like to kill"
 #Killing the pid.
 Stop-Process $PID
